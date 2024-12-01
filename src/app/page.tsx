3b0c237ea22ad1dff5b3a4a15f9b2c8adb5fd96a
Shagun Dubey <1202shagun@gmail.com>
2024-12-01 21:30:33 +0530

    neon postgress and drizzle setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function Home() {
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth();
+
+  if(!session){
+    redirect("/api/auth/signin");
+  }
+
   return (
     <main className=" flex min-h-screen flex-col items-center justify-center py-24">
         <iframe 
