9367107a32a73cb71373c4c71a10f034d33bf7e2
Shagun Dubey <1202shagun@gmail.com>
2024-12-01 22:07:07 +0530

    update

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,7 +1,10 @@
 import Github from "next-auth/providers/github"
 import { NextAuthConfig } from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db/db";
 
 export default {
+  adapter: DrizzleAdapter(db),
   providers: [Github],
   session: {
     strategy: "jwt",
