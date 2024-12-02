4008a40ff6697482833ca292022f8d965008cbb5
Shagun Dubey <1202shagun@gmail.com>
2024-12-02 18:42:06 +0530

    hono js setup

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -8,5 +8,22 @@ export default {
   providers: [Github],
   session: {
     strategy: "jwt",
-  }
+  },
+   callbacks: {
+  async session({ session, token }) {
+    console.log("first")
+    if (token.sub) {
+      session.user.id = token.sub as string; // Ensure the value is a string
+    } else {
+      session.user.id = ""; // Provide a default value if token.sub is undefined
+    }
+    return session;
+  },
+  async jwt({ token, user }) {
+    if (user) {
+      token.sub = user.id;
+    }
+    return token;
+  },
+},
 } satisfies NextAuthConfig;
