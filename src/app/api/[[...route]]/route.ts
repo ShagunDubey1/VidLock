5fdc478bb6d5a342bc200b64742898fbd0f9efab
Shagun Dubey <1202shagun@gmail.com>
2024-12-03 22:18:53 +0530

    hono rpc update

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { Context, Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { AuthConfig, initAuthConfig} from "@hono/auth-js"
+import authConfig from '@/auth.config'
+
+// routes
+import userRoutes from "@/app/api/[[...route]]/user"
 
 export const runtime = 'nodejs'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getAuthConfig(c: Context): AuthConfig{
+  return {
+    secret: process.env.AUTH_SECRET,
+    ...authConfig
+  }
+}
+
 const app = new Hono().basePath('/api')
+
+const routes = app.route("/user", userRoutes);
+
+app.use("*", initAuthConfig(getAuthConfig))
 
 app.get('/hello', (c) => {
   return c.json({
@@ -15,3 +32,5 @@ export const GET = handle(app)
 export const POST = handle(app)
 export const PATCH = handle(app)
 export const DELETE = handle(app)
+
+export type AppType = typeof routes;
